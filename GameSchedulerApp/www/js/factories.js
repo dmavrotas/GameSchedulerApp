4a2b50c56dd4b859565433877654824cddd20505
DMavrotas <dmavrotas@hotmail.com>
2016-02-12 05:23:09 +0200

    Factories

---
+++ GameSchedulerApp/www/js/factories.js
@@ -0,0 +1,41 @@
+﻿angular.modules('starter.services', [])
+
+.factory('GetGamesList', function () {
+
+})
+
+.factory('GetGame', function () {
+
+})
+
+.factory('GetArt', function () {
+
+})
+
+.factory('GetPlatformsList', function () {
+
+})
+
+.factory('GetPlatform', function () {
+
+})
+
+.factory('GetPlatformGames', function () {
+
+})
+
+.factory('PlatformGames', function () {
+
+})
+
+.factory('Updates', function () {
+
+})
+
+.factory('UserRating', function () {
+
+})
+
+.factory('UserFavorites', function () {
+
+});
