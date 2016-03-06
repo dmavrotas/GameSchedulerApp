34d659f6de7b4ecc4b2a50cfb909bd5d40b81114
DMavrotas <dmavrotas@hotmail.com>
2016-03-06 18:14:40 +0100

    Refactored factories, still way to go.

--- GameSchedulerApp/www/js/controllers.js
+++ GameSchedulerApp/www/js/controllers.js
@@ -1,16 +1,23 @@
-angular.module('starter.controllers', [])
+angular.module('gamescheduler.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
+  $scope.remove = function( chat) {
     Chats.remove(chat);
   }
 })
 
-.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats, GamesDB) {
   $scope.chat = Chats.get($stateParams.chatId);
+  //GamesDB.GetPlatformsList()
+  //  .success(function (resp) {
+  //    console.log(resp.data);
+  //  })
+  //  .error(function (error) {
+  //    console.log(error.message);
+  //  });
 })
 
 .controller('AccountCtrl', function($scope) {
